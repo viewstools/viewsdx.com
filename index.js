dea3f5f5b47215697b8342c50fbdea8c163401b6
Tom Parandyk <tom@uxtemple.com>
2017-04-30 00:01:33 +0200

    updated content and removed more

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 import Home from './Home.view'
-import More from './More.view'
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { render } from 'react-dom'
 
 render(
   <Router>
-    <div style={{
-      flexDirection: 'row',
-    }}>
-      <Route path='/' component={Home} />
-      <Route path='/more' component={More} />
-    </div>
+    <Route path='/' component={Home} />
   </Router>,
   document.getElementById('root')
 )
